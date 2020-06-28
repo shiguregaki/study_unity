7c0ae0c2a2fbc26758b09e67da5e5558e21aef2b
shiguregaki <shiguregaki.gmail>
2020-06-29 00:14:55 +0900

    【更新】ByScriptを追加

---
+++ 01_SpringPushStick/Assets/Scripts/StickControllerByScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickControllerByScript : MonoBehaviour
+{
+    Vector3 startPos, startMousePos;
+    float springRate = 150f;
+
+    Rigidbody rb;
+    float forceRate = 5.0f;
+
+    void Start()
+    {
+        startPos = transform.position;
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // バネの力
+        Vector3 stringForce = - springRate * (transform.position - this.startPos);
+        rb.AddForce(stringForce);
+
+        // スワイプの長さを求める
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.startMousePos = Input.mousePosition;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            Vector3 currentMousePos = Input.mousePosition;
+            float force_y = this.forceRate * (currentMousePos.y - this.startMousePos.y);
+            Vector3 force = new Vector3(0.0f, force_y, 0.0f);
+            rb.AddForce(force);
+        }
+    }
+}
--- 01_SpringPushStick/Assets/Scripts/StickControllerBySpringJoint.cs
+++ 01_SpringPushStick/Assets/Scripts/StickControllerBySpringJoint.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StickController : MonoBehaviour
+public class StickControllerBySpringJoint : MonoBehaviour
 {
     Vector3 startPos;
     Rigidbody rb;
