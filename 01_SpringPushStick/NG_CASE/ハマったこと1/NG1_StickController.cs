85820509e4e65e21ca96f0cc88fbd6dfe83a9e81
shiguregaki <shiguregaki.gmail>
2020-06-28 22:23:46 +0900

    【更新】Spring Jointを使った方法でばねの動きを実現

--- 01_SpringPushStick/NG_CASE/ハマったこと1/NG1_StickController.cs
+++ 01_SpringPushStick/NG_CASE/ハマったこと1/NG1_StickController.cs
@@ -6,15 +6,19 @@ public class StickController : MonoBehaviour
 {
     Vector3 startPos;
     Rigidbody rb;
-    float forceRate = 10.0f;
+    float forceRate = 1.0f;
+    SpringJoint springJoint;
 
     void Start()
     {
         rb = this.GetComponent<Rigidbody>();
+        springJoint = GetComponent<SpringJoint>();
     }
 
     void Update()
     {
+        Debug.Log(springJoint.currentForce);
+
         // スワイプの長さを求める
         if (Input.GetMouseButtonDown(0))
         {
@@ -24,7 +28,7 @@ public class StickController : MonoBehaviour
             Vector3 currentPos = Input.mousePosition;
             float force_y = this.forceRate * (currentPos.y - this.startPos.y);
             Vector3 force = new Vector3(0.0f, force_y, 0.0f);
-            rb.AddForce(force);
+            rb.AddForce(force - springJoint.currentForce);
         }
     }
 }
