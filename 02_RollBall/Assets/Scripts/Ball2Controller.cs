ffc64de2f80fdfb814e8881eb0a6e04e2eedb819
shiguregaki <shiguregaki.gmail>
2020-07-01 06:50:04 +0900

    【新規】ビー玉の動きについての調査結果を反映

---
+++ 02_RollBall/Assets/Scripts/Ball2Controller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball2Controller : MonoBehaviour
+{
+    public float speed = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody rb = this.GetComponent<Rigidbody>();
+        Vector3 force = new Vector3(-10.0f, 0.0f, 0.0f) * speed;
+        rb.AddForce(force);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
